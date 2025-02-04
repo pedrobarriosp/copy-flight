764ff30eca4da0c804e03c3033161762a3207755
Pedro Barrios <51805154+pedrobarriosp@users.noreply.github.com>
2025-02-04 11:13:28 +0400

    Update map-navigation

--- src/components/map-navigation.jsx
+++ src/components/map-navigation.jsx
@@ -1,20 +1,108 @@
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardActionArea from '@mui/material/CardActionArea';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+import LightMapImage from '../assets/staticmap-light.png'
 
-import LightWorldMap from '../assets/staticmap-light.png'
+const image = {
+  src: LightMapImage,
+  title: 'Explore destinations',
+  width: '100%',
+}
+
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: 'relative',
+  height: 200,
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important', // Overrides inline-style
+    height: 100,
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiImageBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiImageMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      border: '4px solid currentColor',
+    },
+  },
+}));
+
+const ImageSrc = styled('span')({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center 40%',
+});
+
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}));
+
+const ImageMarked = styled('span')(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}));
 
 export default function Map_Navigation() {
   return (
-    <Card sx={{ minWidth:1 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="300"
-          image={LightWorldMap}
-          alt="world map"
-        />
-      </CardActionArea>
-    </Card>
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
+      <ImageButton
+        focusRipple
+        key={image.title}
+        style={{
+          width: image.width,
+        }}
+      >
+        <ImageSrc style={{ backgroundImage: `url(${image.src})` }} />
+        <ImageBackdrop className="MuiImageBackdrop-root" />
+        <Image>
+          <Typography
+            component="span"
+            variant="subtitle1"
+            color="inherit"
+            sx={(theme) => ({
+              position: 'relative',
+              p: 4,
+              pt: 2,
+              pb: `calc(${theme.spacing(1)} + 6px)`,
+            })}
+          >
+            {image.title}
+            <ImageMarked className="MuiImageMarked-root" />
+          </Typography>
+        </Image>
+      </ImageButton>
+    </Box>
   );
 }
