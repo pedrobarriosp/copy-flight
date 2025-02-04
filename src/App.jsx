752a05ac7f6f21b88e4706b1d74a04021de7201e
Pedro Barrios <51805154+pedrobarriosp@users.noreply.github.com>
2025-02-04 10:32:10 +0400

    Implement created components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
-import { useState } from 'react'
+import Divider from '@mui/material/Divider';
+import FAQ_Accordion from './components/faq-accordion'
+import Tips_Tabs from './components/tips-tabs'
+import PopularRoutes_Grid from './components/popularRoutes-grid';
+import Configuration_Footer from './components/setup-footer';
+import Map_Navigation from './components/map-navigation';
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <div>
+        <Map_Navigation/>
+      </div>
+      <Divider/>
+      <div>
+        <span>
+          Useful tools to help you find the best deals
+        </span>
+        <Tips_Tabs/>
+      </div>
+      <Divider/>
+      <div>
+        <span>
+          Frequently Asked Questions
+        </span>
+        <FAQ_Accordion/>
+      </div>
+      <Divider/>
+      <div>
+        <span>
+          Find Cheap Flights on Popular Routes
+        </span>
+        <PopularRoutes_Grid/>
+      </div>
+      <Divider/>
+      <div>
+        <Configuration_Footer/>
+      </div>
+    </div>
   )
 }
 
